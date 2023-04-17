54a15b664b2e99fe1a8c4876e4a5baffa26c217d
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-04-17 11:01:54 +0200

    Update BrickOTest.java

--- src/main/java/BrickOTest.java
+++ src/main/java/BrickOTest.java
@@ -2,6 +2,7 @@ import org.abstractica.javacsg.JavaCSG;
 import org.abstractica.javacsg.JavaCSGFactory;
 
 public class BrickOTest {
+
     public static void main(String[] args) {
         JavaCSG csg = JavaCSGFactory.createDefault();
         BrickO circle = new BrickO(csg, 30, 6, 360);
