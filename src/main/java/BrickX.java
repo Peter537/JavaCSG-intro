1d6201bae507b75a978af58624baa6571ce72316
Jeanette93Cph <jeanette.93@live.dk>
2023-04-17 11:10:39 +0200

    updated brickX
    updated brickX

--- src/main/java/BrickX.java
+++ src/main/java/BrickX.java
@@ -1,8 +1,36 @@
+import org.abstractica.javacsg.Geometry3D;
 import org.abstractica.javacsg.JavaCSG;
+import org.abstractica.javacsg.JavaCSGFactory;
 
 public class BrickX {
 
-    public BrickX(JavaCSG csg) {
+    JavaCSG csg = JavaCSGFactory.createDefault();
+
+    int length;
+    int height;
+    int width;
+
+
+    public BrickX(JavaCSG csg,int length, int height, int width) {
+
+        this.csg = csg;
+        this.length = length;
+        this.height = height;
+        this.width = width;
+    }
+
+
+    public Geometry3D generateBrickX()
+    {
+        Geometry3D cube1 = csg.box3D(30, 5, 6, true);
+        var rotatecube2 = csg.rotate3DX(csg.degrees(50));
+        Geometry3D cube2 = csg.box3D(5, 30, 6, true);
+        var shapeBrick = csg.union3D(cube1, cube2);
+
+        return shapeBrick;
 
     }
+
+
+
 }
--- src/main/java/BrickXTest.java
+++ src/main/java/BrickXTest.java
@@ -1,6 +1,15 @@
+import org.abstractica.javacsg.Geometry3D;
+import org.abstractica.javacsg.JavaCSG;
+import org.abstractica.javacsg.JavaCSGFactory;
+
 public class BrickXTest {
 
     public static void main(String[] args) {
 
+        JavaCSG csg = JavaCSGFactory.createDefault();
+        BrickX brickx = new BrickX(csg, 30, 30, 30);
+        csg.view(brickx.generateBrickX());
+
     }
+
 }
