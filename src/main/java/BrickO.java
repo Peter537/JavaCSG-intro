2efe8387ccea84763b654e3b79038b3b774198de
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-04-17 11:01:42 +0200

    Update BrickO.java

--- src/main/java/BrickO.java
+++ src/main/java/BrickO.java
@@ -5,23 +5,19 @@ public class BrickO {
 
     private final JavaCSG csg;
     private final int diameter;
-
     private final int height;
-    private final int angularREsolution;
+    private final int angularResolution;
 
-
-    public BrickO(JavaCSG csg, int diameter, int height, int angularREsolution) {
+    public BrickO(JavaCSG csg, int diameter, int height, int angularResolution) {
         this.csg = csg;
         this.diameter = diameter;
         this.height = height;
-        this.angularREsolution = angularREsolution;
+        this.angularResolution = angularResolution;
     }
 
     public Geometry3D generatedCircle() {
-        Geometry3D circle = csg.cylinder3D(diameter, height, angularREsolution, true);
-
-        Geometry3D centerHole = csg.cylinder3D(diameter / 2, height + 50, angularREsolution, true);
-
+        Geometry3D circle = csg.cylinder3D(diameter, height, angularResolution, true);
+        Geometry3D centerHole = csg.cylinder3D(diameter / 2.0, height + 50, angularResolution, true);
         return csg.difference3D(circle, centerHole);
     }
 }
