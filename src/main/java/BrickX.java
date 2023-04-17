6b88fe3069975251defde86fe198d8978aa91b40
Peter537 <52176852+Peter537@users.noreply.github.com>
2023-04-17 12:02:19 +0200

    update brick X

--- src/main/java/BrickX.java
+++ src/main/java/BrickX.java
@@ -4,31 +4,21 @@ import org.abstractica.javacsg.JavaCSGFactory;
 
 public class BrickX {
 
-    JavaCSG csg = JavaCSGFactory.createDefault();
+    private final JavaCSG csg;
+    private final int length;
+    private final int height;
+    private final int width;
 
-    int length;
-    int height;
-    int width;
-
-
-    public BrickX(JavaCSG csg,int length, int height, int width) {
-
+    public BrickX(JavaCSG csg, int length, int width, int height) {
         this.csg = csg;
         this.length = length;
-        this.height = height;
         this.width = width;
+        this.height = height;
     }
 
-
-    public Geometry3D generateBrickX()
-    {
-        Geometry3D cube1 = csg.box3D(30, 5, 6, true);
-        var rotatecube2 = csg.rotate3DX(csg.degrees(50));
-        Geometry3D cube2 = csg.box3D(5, 30, 6, true);
-        var shapeBrick = csg.union3D(cube1, cube2);
-        return shapeBrick;
+    public Geometry3D generate() {
+        Geometry3D cube1 = csg.box3D(length, width, height, true);
+        Geometry3D cube2 = csg.box3D(width, length, height, true);
+        return csg.union3D(cube1, cube2);
     }
-
-
-
 }
--- src/main/java/BrickXTest.java
+++ src/main/java/BrickXTest.java
@@ -1,15 +1,11 @@
-import org.abstractica.javacsg.Geometry3D;
 import org.abstractica.javacsg.JavaCSG;
 import org.abstractica.javacsg.JavaCSGFactory;
 
 public class BrickXTest {
 
     public static void main(String[] args) {
-
         JavaCSG csg = JavaCSGFactory.createDefault();
-        BrickX brickx = new BrickX(csg, 30, 30, 30);
-        csg.view(brickx.generateBrickX());
-
+        BrickX brickx = new BrickX(csg, 30, 5, 6);
+        csg.view(brickx.generate());
     }
-
 }
